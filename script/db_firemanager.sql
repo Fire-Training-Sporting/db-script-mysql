82a45174f755e4fc922fc6d82fcce7e1dcc9d49e
Vitor-Gomesz <vitor.golieveira@sptech.school>
2026-04-04 02:58:13 -0300

    Alteração do bd

--- script/db_firemanager.sql
+++ script/db_firemanager.sql
@@ -2,12 +2,12 @@ CREATE DATABASE db_firemanager;
 USE db_firemanager;
 
 CREATE TABLE tb_tipo_usuarios(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id bigint PRIMARY KEY AUTO_INCREMENT,
 cargo VARCHAR(25) NOT NULL UNIQUE
 );
 
 CREATE TABLE tb_condominios(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id bigint PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 cidade VARCHAR(60) NOT NULL,
 bairro VARCHAR(60) NOT NULL,
@@ -16,13 +16,13 @@ numero VARCHAR(5) NOT NULL
 );
 
 CREATE TABLE tb_usuarios(
-id INT PRIMARY KEY AUTO_INCREMENT,
-fk_tipo_usuario INT NOT NULL,
+id bigint PRIMARY KEY AUTO_INCREMENT,
+fk_tipo_usuario bigint NOT NULL,
 nome VARCHAR(60) NOT NULL UNIQUE,
 email VARCHAR(60) NOT NULL UNIQUE,
 telefone CHAR(11) NOT NULL UNIQUE,
 senha VARCHAR(255) NOT NULL,
-fk_condominio INT,
+fk_condominio bigint,
 criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT chk_email CHECK (email LIKE '%@%'),
 CONSTRAINT fk_tipo_usuario FOREIGN KEY (fk_tipo_usuario) REFERENCES tb_tipo_usuarios(id),
@@ -30,13 +30,13 @@ CONSTRAINT fk_condominio_usuario FOREIGN KEY (fk_condominio) REFERENCES tb_condo
 );
 
 CREATE TABLE tb_permissoes(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id bigint PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL UNIQUE
 );
 
 CREATE TABLE tb_acessos(
-id_usuario INT NOT NULL,
-id_permissao INT NOT NULL,
+id_usuario bigint NOT NULL,
+id_permissao bigint NOT NULL,
 data_acesso DATETIME DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT pk_composta_usuario_permissao PRIMARY KEY (id_usuario, id_permissao),
 CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES tb_usuarios(id),
@@ -44,14 +44,15 @@ CONSTRAINT fk_permissao FOREIGN KEY (id_permissao) REFERENCES tb_permissoes(id)
 );
 
 CREATE TABLE tb_servicos(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id bigint PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL UNIQUE
 );
 
 CREATE TABLE tb_saldo_servicos(
-id INT PRIMARY KEY AUTO_INCREMENT,
-fk_servico INT NOT NULL,
-fk_usuario INT NOT NULL,
+id bigint PRIMARY KEY AUTO_INCREMENT,
+fk_servico bigint NOT NULL,
+fk_usuario bigint NOT NULL,
+quantidade int not null,
 status_saldo ENUM('valido', 'pendente', 'expirado') NOT NULL,
 data_compra DATETIME DEFAULT CURRENT_TIMESTAMP,
 data_validade DATE NOT NULL,
@@ -59,19 +60,34 @@ CONSTRAINT fk_servico_saldo FOREIGN KEY (fk_servico) REFERENCES tb_servicos(id),
 CONSTRAINT fk_servico_usuario FOREIGN KEY (fk_usuario) REFERENCES tb_usuarios(id)
 );
 
+
 CREATE TABLE tb_agendamentos(
-id INT PRIMARY KEY AUTO_INCREMENT,
-fk_usuario INT NOT NULL,
-dados_agendamento JSON NOT NULL,
-data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-data_update DATETIME,
-CONSTRAINT fk_usuario_agendamento FOREIGN KEY (fk_usuario) REFERENCES tb_usuarios(id)
+id bigint PRIMARY KEY AUTO_INCREMENT,
+fk_aluno bigint not null,
+fk_professor bigint not null,
+fk_auxiliar bigint,
+fk_servico bigint not null,
+fk_condominio bigint not null,
+data_agendamento date not null,
+hora_inicio time not null,
+observacao varchar(255),
+status enum('pendente', 'confirmado', 'cancelado', 'finalizado') default 'pendente',
+criado_em datetime default current_timestamp,
+atualizado_em datetime default current_timestamp,
+constraint fk_agendamento_aluno foreign key (fk_aluno) references tb_usuarios(id),
+constraint fk_agendamento_professor foreign key (fk_professor) references tb_usuarios(id),
+constraint fk_agendamento_auxiliar foreign key (fk_auxiliar) references tb_usuarios(id),
+constraint fk_agendamento_servico foreign key (fk_servico) references tb_servicos(id),
+constraint fk_agendamwnto_condominio foreign key (fk_condominio) references tb_condominios(id)
 );
 
 show tables;
+
 
 INSERT INTO tb_tipo_usuarios (cargo) VALUES
 ('root'),
 ('administracao'),
 ('professor'),
 ('aluno');
+
+select * from tb_usuarios;
